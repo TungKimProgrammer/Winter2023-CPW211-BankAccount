3350cb964fa9192318781115e3f04f3feb785edb
TUNG N KIM <Tungkims@gmail.com>
2023-02-14 04:09:59 -0600

    Implemented Owner property tests

--- BankAccount/Account.cs
+++ BankAccount/Account.cs
@@ -11,6 +11,8 @@ namespace BankAccount
     /// </summary>
     public class Account
     {
+        private string owner;
+
         /// <summary>
         /// Create an account with a specific owner and a balance of 0
         /// </summary>
@@ -23,7 +25,56 @@ namespace BankAccount
         /// <summary>
         /// The account holders full name (first and last)
         /// </summary>
-        public string Owner { get; set; }
+        public string Owner 
+        { 
+            get { return owner; }
+            set 
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException($"{Owner} cannot be null");
+                }
+
+                if (value.Trim() == string.Empty)
+                {
+                    throw new ArgumentException($"{nameof(Owner)} must have some text");
+                }
+
+                if (IsOwnerNameValid(value))
+                {
+                    owner = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"{nameof(Owner)} can be up to 20 characters, A-Z/space only");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Checks if Owner name is less than or equal to 20 characters, A-Z and whitespace chracters are allowed
+        /// </summary>
+        /// <returns></returns>
+        private bool IsOwnerNameValid(string ownerName)
+        {
+            char[] validCharacters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+            const int MaxLengthOwnerName = 20;
+            ownerName = ownerName.ToLower();
+            if (ownerName.Length > MaxLengthOwnerName)
+            {
+                return false;
+            }
+            
+            foreach (char letter in ownerName)
+            {
+                if (letter != ' ' && !validCharacters.Contains(letter))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 
         /// <summary>
         /// The amount of money surrently in the account
--- BankAccountTests/AccountTests.cs
+++ BankAccountTests/AccountTests.cs
@@ -16,7 +16,7 @@ namespace BankAccount.Tests
         [TestInitialize]
         public void CreateDefaultAccount()
         {
-            acc = new Account("J. Doe");
+            acc = new Account("J Doe");
         }
 
         [TestMethod()]
@@ -24,6 +24,7 @@ namespace BankAccount.Tests
         [DataRow(0.01)]
         [DataRow(1.99)]
         [DataRow(9_999.99)]
+        [TestCategory("Deposit")]
         public void Deposit_APositiveAmount_AddToBalance(double aPositiveAmount)
         {
             // Account acc = new Account("J Doe");
@@ -33,6 +34,7 @@ namespace BankAccount.Tests
         }
 
         [TestMethod]
+        [TestCategory("Deposit")]
         public void Deposit_ApositiveAmount_ReturnUpdatedBalance()
         {
             // AAA - Arrange Act Assert
@@ -51,6 +53,7 @@ namespace BankAccount.Tests
         [TestMethod]
         [DataRow(-1)]
         [DataRow(0)]
+        [TestCategory("Deposit")]
         public void Deposit_ZeroOrLess_ThrowsArgumentOutOfRangeException(double invalidDepositAmount)
         {
             // Arrange
@@ -63,6 +66,7 @@ namespace BankAccount.Tests
         
         [TestMethod]
         [DataRow(50)]
+        [TestCategory("Withdraw")]
         public void Withdraw_APositiveAmount_DecreasesBalance(double aPositiveAmount)
         {
             // Arrange
@@ -82,6 +86,7 @@ namespace BankAccount.Tests
         [TestMethod]
         [DataRow(100, 50)]
         [DataRow(100, .99)]
+        [TestCategory("Withdraw")]
         public void Withdraw_APositiveAmount_ReturnsUpdatedBalance(double initialDeposit, double aPositiveAmount)
         {
             // Arrange
@@ -99,6 +104,7 @@ namespace BankAccount.Tests
         [DataRow(0)]
         [DataRow(-.01)]
         [DataRow(-10)]
+        [TestCategory("Withdraw")]
         public void Withdraw_ZeroOrLess_ThrowsArgumentOutOfRangeException(double zeroOrLess)
         {
             // Arrange
@@ -112,6 +118,7 @@ namespace BankAccount.Tests
 
         [TestMethod]
         [DataRow(1000)]
+        [TestCategory("Withdraw")]
         public void Withdraw_MoreThanAvailableBalance_ThrowsArgumentException(double moreThanAvailableBalance)
         {
             // Arrange
@@ -121,6 +128,52 @@ namespace BankAccount.Tests
             // Assert
             Assert.ThrowsException<ArgumentException>(() => acc.Withdraw(moreThanAvailableBalance));
 
+        }
+
+        [TestMethod]
+        [TestCategory("Owner")]
+        public void Owner_SetAsNull_ThrowArgumentNullException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.ThrowsException<ArgumentNullException>(() => acc.Owner = null);
+        }
+
+        [TestMethod]
+        [TestCategory("Owner")]
+        public void Owner_SetAsWhiteSpaceOrEmptyString_ThrowArgumentException()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+            Assert.ThrowsException<ArgumentException>(() => acc.Owner = String.Empty);
+            Assert.ThrowsException<ArgumentException>(() => acc.Owner = " ");
+        }
+
+        [TestMethod]
+        [DataRow("Tung")]
+        [DataRow("Tung Kim")]
+        [DataRow("Tung Ngoc Kim Smart")]
+        [TestCategory("Owner")]
+        public void Owner_SetAsUpTo20Chracters_SetsSuccessfully(string ownerName)
+        {
+            acc.Owner = ownerName;
+            Assert.AreEqual(ownerName, acc.Owner);
+        }
+
+        [TestMethod]
+        [DataRow("Tung 5th")]
+        [DataRow("Tung Kim A Student at CPTC")]
+        [DataRow("$$$###")]
+        [TestCategory("Owner")]
+        public void Owner_InvalidOwnerName_ThrowArgumentException(string ownerName)
+        {
+            Assert.ThrowsException<ArgumentException>(() => acc.Owner = ownerName);
         }
     }
 
